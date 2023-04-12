95e679b6d4f65dae900b78cceffefa58a091f0ff
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-12 21:00:36 +0600

    first commit

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD5kH_wqfQm1MTZ5VdGFaUJ-4sD4KFLlQ4",
+    authDomain: "simple-firebase-204ac.firebaseapp.com",
+    projectId: "simple-firebase-204ac",
+    storageBucket: "simple-firebase-204ac.appspot.com",
+    messagingSenderId: "880794446439",
+    appId: "1:880794446439:web:4856d8e7ed310fa3b282c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/__steps.js
@@ -0,0 +1,3 @@
+// 1. Visit console.firebase.google.com
+// 2. Create project (Skip google Analytics)
+// 3. Register App (Create config)
